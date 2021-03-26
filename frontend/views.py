38c7403b3c8c916b5d43754b5bbbb47dfc8d718a
Kirankumar Gadagi <kirankumar.gadagi@iping.in>
2021-03-26 18:08:39 +0530

    API

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
 # Create your views here.
+from todos.models import Todo
+
+
+def index(request):
+    return render(request, 'frontend/index.html')
+
+
+class TodoDetailView(DetailView):
+    model = Todo
+    template_name = "frontend/index.html"
